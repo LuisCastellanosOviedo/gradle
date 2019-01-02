2cde77e14d38f3e078b9f13db0ff97e570142fd5
Luis <luis.castellanos.oviedo@gmail.com>
2019-01-02 18:10:12 -0500

    classes and package

--- javaProject/src/main/java/com/gradle/tasks/todo/repository/InMemoryToDoRepository.java
+++ javaProject/src/main/java/com/gradle/tasks/todo/repository/InMemoryToDoRepository.java
@@ -1,4 +1,32 @@
 package com.gradle.tasks.todo.repository;
 
-public class InMemoryToDoRepository {
+import com.gradle.tasks.todo.model.ToDoItem;
+
+import java.util.List;
+
+public class InMemoryToDoRepository implements ToDoRepository {
+    @Override
+    public List<ToDoItem> findAll() {
+        return null;
+    }
+
+    @Override
+    public ToDoItem findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Long insert(ToDoItem toDoItem) {
+        return null;
+    }
+
+    @Override
+    public void update(ToDoItem toDoItem) {
+
+    }
+
+    @Override
+    public void delete(ToDoItem toDoItem) {
+
+    }
 }
--- javaProject/src/main/java/com/gradle/tasks/todo/repository/ToDoRepository.java
+++ javaProject/src/main/java/com/gradle/tasks/todo/repository/ToDoRepository.java
@@ -1,4 +1,14 @@
 package com.gradle.tasks.todo.repository;
 
-public class ToDoRepository {
+import com.gradle.tasks.todo.model.ToDoItem;
+
+import java.util.List;
+
+public interface ToDoRepository {
+
+    List<ToDoItem> findAll();
+    ToDoItem findById(Long id);
+    Long insert(ToDoItem toDoItem);
+    void update(ToDoItem toDoItem);
+    void delete(ToDoItem toDoItem);
 }
